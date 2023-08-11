b9e0c040a43680727ced87163a81e549bf1294cb
Captain PhoeniX <71826054+cphoenix-07@users.noreply.github.com>
2023-08-11 23:40:28 +0530

    Update server.js

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,4 +1,4 @@
-export const server = "https://poochiee-mvp.onrender.com//api/v2";
+export const server = "https://poochiee-mvp.onrender.com/api/v2";
 
 export const backend_url = "https://poochiee-mvp.onrender.com/";
 
